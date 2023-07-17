59c41e1adae9543a79781d51e9af1c8c1234273d
Shtekata <gesheval@gmail.com>
2023-07-17 21:57:43 +0300

    Using clsx

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,13 @@ import Head from 'next/head'
 import Layout, {siteTitle} from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
+import Alert from '../components/Alert'
 
 let home = true
+let type = 'success'
 const change = home => {
   home == true ? (home = false) : (home = true)
+  type == 'success' ? (type = 'error') : (type = 'success')
   return home
 }
 
@@ -19,16 +22,18 @@ export default function Home() {
         <Head>
           <title>{siteTitle}</title>
         </Head>
-        <section className={utilStyles.headingMd}>
-          <p>
-            Hello, I'm Ghu. I'm a software enginer and a translator (English/Japanese). You can contact me on{' '}
-            <a href=''>Twetter</a>.
-          </p>
-          <p>
-            (This is a sample website - you'll be building a site like this on{' '}
-            <a href='https://nextjs.org/learn'>our Next.js tutorial</a>.)
-          </p>
-        </section>
+        <Alert type={type}>
+          <section className={utilStyles.headingMd}>
+            <p>
+              Hello, I'm Ghu. I'm a software enginer and a translator (English/Japanese). You can contact me on{' '}
+              <a href=''>Twetter</a>.
+            </p>
+            <p>
+              (This is a sample website - you'll be building a site like this on{' '}
+              <a href='https://nextjs.org/learn'>our Next.js tutorial</a>.)
+            </p>
+          </section>
+        </Alert>
       </Layout>
     </>
   )
