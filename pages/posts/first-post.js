1340e26a967bb4bf25a1b52eca45aad4829d0f98
Shtekata <gesheval@gmail.com>
2023-08-05 18:26:53 +0300

    Add forms with html validations

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -10,6 +10,29 @@ export default function Post() {
         <title>First Post</title>
       </Head>
       <h1>First Post</h1>
+      <form action='/' method='post'>
+        <label for='first'>First name:</label>
+        <br />
+        <input type='text' id='first' name='first' />
+        <br />
+        <label for='last'>Last name:</label>
+        <br />
+        <input type='text' id='last' name='last' />
+        <br />
+        <button type='submit'>Submit</button>
+      </form>
+      <br />
+      <form action='/' method='post'>
+        <label for='roll'>Roll Number</label>
+        <br />
+        <input type='text' id='roll' name='roll' required minlength='10' maxlength='20' />
+        <br />
+        <label for='name'>Name:</label>
+        <br />
+        <input type='text' id='name' name='name' required />
+        <br />
+        <button type='submit'>Submit</button>
+      </form>
       <h2>
         <Link href='/'>Back to home</Link>
       </h2>
