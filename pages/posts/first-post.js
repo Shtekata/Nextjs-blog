608327f200e1ab4abed1eb5b68833978261cceb3
Shtekata <gesheval@gmail.com>
2023-08-14 20:08:21 +0300

    Add 'First Post' forms.

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -2,8 +2,34 @@ import Link from 'next/link'
 import Head from 'next/head'
 import Layout from '../../components/layout'
 import {change, home} from '..'
+import React, {useState, useEffect} from 'react'
 
 export default function Post() {
+  const [count, setCount] = useState(0)
+  const [name, setName] = useState('')
+  const [rollNumber, setRollNumber] = useState('')
+  useEffect(() => {
+    document.title = 'You clicked ' + count + ' times'
+  })
+
+  function validateFormWithJS(event) {
+    event.preventDefault()
+    if (!name && rollNumber.length < 3) {
+      alert('Please enter your name and roll number should be at least 3 digits long.')
+      return
+    }
+    if (!name) {
+      alert('Please enter your name.')
+      return
+    }
+    if (rollNumber.length < 3) {
+      alert('Roll number should be at least 3 digits long.')
+      return
+    }
+    setName('')
+    setRollNumber('')
+  }
+
   return (
     <Layout ch={change} ho={home}>
       <Head>
@@ -11,11 +37,11 @@ export default function Post() {
       </Head>
       <h1>First Post</h1>
       <form action='/' method='post'>
-        <label for='first'>First name:</label>
+        <label htmlFor='first'>First name:</label>
         <br />
         <input type='text' id='first' name='first' />
         <br />
-        <label for='last'>Last name:</label>
+        <label htmlFor='last'>Last name:</label>
         <br />
         <input type='text' id='last' name='last' />
         <br />
@@ -23,16 +49,36 @@ export default function Post() {
       </form>
       <br />
       <form action='/' method='post'>
-        <label for='roll'>Roll Number</label>
+        <label htmlFor='roll'>Roll Number</label>
         <br />
-        <input type='text' id='roll' name='roll' required minlength='10' maxlength='20' />
+        <input type='text' id='roll' name='roll' required minLength='10' maxLength='20' />
         <br />
-        <label for='name'>Name:</label>
+        <label htmlFor='name'>Name:</label>
         <br />
         <input type='text' id='name' name='name' required />
         <br />
         <button type='submit'>Submit</button>
       </form>
+      <br />
+      <form onSubmit={validateFormWithJS}>
+        <label htmlFor='rollNumber'>Roll Number:</label>
+        <br />
+        <input
+          onChange={event => setRollNumber(event.target.value)}
+          value={rollNumber}
+          type='text'
+          name='rollNumber'
+          id='rollNumber'
+        />
+        <br />
+        <label htmlFor='name2'>Name:</label>
+        <br />
+        <input onChange={event => setName(event.target.value)} value={name} type='text' name='name2' id='name2' />
+        <br />
+        <button type='submit'>Submit</button>
+      </form>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
       <h2>
         <Link href='/'>Back to home</Link>
       </h2>
