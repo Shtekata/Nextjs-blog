da27cfa831e3e2e46802d8772b952f649f2527aa
Shtekata <gesheval@gmail.com>
2023-08-28 00:35:46 +0300

    Implement err.cancelled

--- pages/_app.js
+++ pages/_app.js
@@ -5,15 +5,23 @@ import {useEffect} from 'react'
 export default function App({Component, pageProps}) {
   const router = useRouter()
 
+  // useEffect(() => {
+  //   const handleRouteChange = (url, {shallow}) =>
+  //     console.log(`App is changing to ${url} ${shallow ? 'with' : 'without'} shallow routing`)
+
+  //   router.events.on('routeChangeStart', handleRouteChange)
+
+  //   return () => {
+  //     router.events.off('routeChangeStart', handleRouteChange)
+  //   }
+  // }, [router])
+
   useEffect(() => {
-    const handleRouteChange = (url, {shallow}) =>
-      console.log(`App is changing to ${url} ${shallow ? 'with' : 'without'} shallow routing`)
-
-    router.events.on('routeChangeStart', handleRouteChange)
-
-    return () => {
-      router.events.off('routeChangeStart', handleRouteChange)
+    const handleRouteChangeError = (err, url) => {
+      if (err.cancelled) console.log(`Route to ${url} was cancelled`)
     }
+    router.events.on('routeChangeError', handleRouteChangeError)
+    return () => router.events.off('routeChangeError', handleRouteChangeError)
   }, [router])
 
   // return <Component key={router.asPath} {...pageProps} />
